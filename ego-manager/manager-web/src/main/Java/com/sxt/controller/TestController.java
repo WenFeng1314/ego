6ad074e12ada432239c14c1de80cbd5a7b4b991c
文丰 <839541645@qq.com>
2019-08-05 22:30:48 +0800

    提交了，TestController

---
+++ ego-manager/manager-web/src/main/Java/com/sxt/controller/TestController.java
@@ -0,0 +1,14 @@
+package com.sxt.controller;
+
+/**
+ * @author WWF
+ * @title: TestController
+ * @projectName ego
+ * @description: com.sxt.controller
+ * @date 2019/8/5 22:29
+ */
+public class TestController {
+    public static void main(String[] args) {
+        System.out.println("测试!!!");
+    }
+}
